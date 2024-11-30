4cb8cff315fd74f73f318c4f143eeaf011287ca7
Antoine Price <73240970+AntP1997@users.noreply.github.com>
2024-11-30 11:10:26 -0800

    Update Main.kt

--- Main.kt
+++ Main.kt
@@ -144,7 +144,7 @@ fun FindUserName(inputID: String, User_Attempt : Int)
         //this adds one to keep count with user input
         val ADD = 1
 
-        // this is the number of tries for your data
+        // this is the number of tries for you to access this data
         val MAX_TRIES = 2
 
 
